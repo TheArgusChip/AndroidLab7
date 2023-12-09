b232138048348d522a5bb24bda5c4b464a0b9864
Артём Рассудов <mr.rassudov@mail.ru>
2023-12-09 09:50:59 +0700

    late init Забыл создать репозиторий. - Парсер JSON

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=2a294b3e669e61d193480975845f9972" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
